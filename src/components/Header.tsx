4a6e592f68d5c8ae69094b07bfca36aadacf36fa
jerwintuchi <jerwin.tuchi3@gmail.com>
2025-02-25 16:53:27 +0800

    ui: stylized the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 
 export default function Header() {
     return (
-        <header className="fixed top-0 left-0 w-full bg-blue-900 py-4 shadow-md z-50">
+        <header className="fixed top-0 left-1/2 transform -translate-x-1/2 bg-blue-900 py-4 shadow-md z-50 w-auto max-w-7xl rounded-b-full">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-center">
-                <nav className="h-16 flex items-center ">
+                <nav className="h-8 flex items-center ">
                     <ul className="flex gap-6 px-6">
                         <li>
                             <Link
