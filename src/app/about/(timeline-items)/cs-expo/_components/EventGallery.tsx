c250af77deffe19ba06948a13dba3a0c8602d2cb
jerwintuchi <jerwin.tuchi3@gmail.com>
2025-03-01 16:13:46 +0800

    ui: better scrollindicator

--- src/app/about/(timeline-items)/cs-expo/_components/EventGallery.tsx
+++ src/app/about/(timeline-items)/cs-expo/_components/EventGallery.tsx
@@ -1,30 +1,32 @@
-import { Carousel, CarouselContent, CarouselItem } from '@/components/ui/carousel'
-import React from 'react'
-import Image from 'next/image'
-import { SlidingImages } from './CSExpoClient'
-import ScrollIndicator from './ScrollIndicator'
+import { Carousel, CarouselContent, CarouselItem } from '@/components/ui/carousel';
+import React from 'react';
+import Image from 'next/image';
+import { SlidingImages } from './CSExpoClient';
+import ScrollIndicator from './ScrollIndicator';
 
 export default function EventGalleryCarousel() {
     return (
-        <div className="w-full max-w-2xl px-4">
+        <div className="relative w-full max-w-2xl px-4">
+            {/* Scroll Indicator fixed on top */}
+            <div className="absolute top-2/3 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10 w-full">
+                <ScrollIndicator />
+            </div>
+
             <Carousel>
                 <CarouselContent>
                     {SlidingImages.map((src, index) => (
                         <CarouselItem key={index}>
-                            <div className="relative">
-                                <Image
-                                    width={500}
-                                    height={500}
-                                    src={src}
-                                    alt={`CS Expo Image ${index + 1}`}
-                                    className="rounded-lg w-full h-auto"
-                                />
-                                <ScrollIndicator />
-                            </div>
+                            <Image
+                                width={500}
+                                height={500}
+                                src={src}
+                                alt={`CS Expo Image ${index + 1}`}
+                                className="rounded-lg w-full h-auto"
+                            />
                         </CarouselItem>
                     ))}
                 </CarouselContent>
             </Carousel>
         </div>
-    )
+    );
 }
