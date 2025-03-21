e77080ab3c7149a77f0f8d7167c7c57dc016b58d
jerwintuchi <jerwin.tuchi3@gmail.com>
2025-03-21 19:17:16 +0800

    added forecast client page

---
+++ src/app/forecast/ForecastClient.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ForecastClient() {
+    return (
+        <div className="min-h-screen bg-gradient-to-r from-black to-blue-900 py-24 px-4 relative overflow-hidden text-gray-100">
+            Forecast Client Page
+        </div>
+    )
+}
