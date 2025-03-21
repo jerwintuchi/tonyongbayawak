1ab2e56fac3e823605dcd5793153e1335f0d8ea3
jerwintuchi <jerwin.tuchi3@gmail.com>
2025-03-21 21:49:09 +0800

    setup nextjs rewrite() to point to local fastAPI server

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/py/:path*",  // Next.js API route
+        destination: "http://127.0.0.1:8000/:path*", // FastAPI server
+      },
+    ];
+  },
 };
 
 export default nextConfig;
